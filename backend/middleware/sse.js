bbbf0040a79e0bde531f878404c91f2180c806b2
Zeyoman <101411550+Zeyoman@users.noreply.github.com>
2025-07-24 16:56:38 +0200

    Fix SSE manager and remove express-sse

--- backend/middleware/sse.js
+++ backend/middleware/sse.js
@@ -1,3 +1,41 @@
-// middleware/sse.js
-const SSE = require('express-sse')
-module.exports = new SSE()
+class SSEManager {
+  constructor() {
+    this.clients = new Map();
+  }
+
+  init(req, res, clientId) {
+    res.writeHead(200, {
+      'Content-Type': 'text/event-stream',
+      'Cache-Control': 'no-cache',
+      Connection: 'keep-alive'
+    });
+    const id = clientId || req.userId || Date.now().toString();
+    this.register(id, res);
+    req.on('close', () => {
+      this.unregister(id);
+    });
+  }
+
+  register(id, res) {
+    this.clients.set(id, res);
+  }
+
+  unregister(id) {
+    const res = this.clients.get(id);
+    if (res) {
+      res.end();
+      this.clients.delete(id);
+    }
+  }
+
+  broadcast(data) {
+    const payload = `data: ${JSON.stringify(data)}\n\n`;
+    for (const res of this.clients.values()) {
+      try {
+        res.write(payload);
+      } catch {}
+    }
+  }
+}
+
+module.exports = new SSEManager();
