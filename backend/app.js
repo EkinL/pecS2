1882c346ba833f4a4947b5e2c087d800783839b2
Zeyoman <alex10pump@gmail.com>
2025-07-24 17:42:34 +0200

    validation sse payment psp

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose");
 
 const sequelize = require('./sequelize');
 const allRoutes = require('./routes')
+const sse = require('./sse');
 
 const User = require("./models/user");
 const UserMongo = require("./models/user.mongo");
@@ -18,6 +19,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/', allRoutes)
+app.use('/sse', sse.router)
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
